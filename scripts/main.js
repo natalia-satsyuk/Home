5c61bc58abdb052fa8f163816bbb77acdd60058e
Nataliya Satsyuk <natalia.satsyuk@gmail.com>
2016-07-30 13:55:30 +0300

    make pop-up menu in project ( 768px - 481px)

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,12 @@ $(".header-menu-icon").click(function() {
 $(".menu-list-close").click(function() {
 	$(".header-menu-list").hide(400);
 });
+
+
+$(".project-menu-icon").click(function() {
+	$(".left-up-first-text").slideDown(400);
+});
+
+$(".project-list-close").click(function() {
+	$(".left-up-first-text").slideUp(400);
+});
